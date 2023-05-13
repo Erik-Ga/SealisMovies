9ec7fe22ccdcac4ce6fbb3afb57b42a56fdbd4c5
ErikG <115992708+Erik-Ga@users.noreply.github.com>
2023-05-13 21:59:13 +0200

    added subcategory

---
+++ SealisMovies/SealisMovies/Data/Migrations/20230513194528_subcategoryadd.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SealisMovies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class subcategoryadd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubCategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubCategories_Categories_Id",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+        }
+    }
+}
---
+++ SealisMovies/SealisMovies/Models/SubCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace SealisMovies.Models
+{
+    public class SubCategory
+    {
+        public int Id { get; set; }
+        public string SubCategoryName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
